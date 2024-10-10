89366f1d346a4cee0214394b40959a53c0867167
AvaneeshAsokan <avaneesh@q-devengine.com>
2024-10-10 12:24:04 +0530

    - updating constants with pixel device/room id

--- app/src/main/java/com/qdev/gptjanuswebrtc/Constants.java
+++ app/src/main/java/com/qdev/gptjanuswebrtc/Constants.java
@@ -1,8 +1,8 @@
 package com.qdev.gptjanuswebrtc;
 
 public class Constants {
-    public static String JANUS_SERVER_URI = "wss://jnx13.snipback.com/websocket";
+    public static String JANUS_SERVER_URI = "wss://jnx14.snipback.com/websocket";
     public static String JANUS_PLUGIN_NAME = "janus.plugin.videoroom";
-    public static int ROOM_ID = /*20188*/19478;
+    public static int ROOM_ID = /*20188*/21999;
     public static String SECRET = "YMDhSqL695vGDUELODmxWy3eA";
 }
